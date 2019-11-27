c8cb0ec32dac410cbedac37f1d6e005869916e8b
Yura Govorushkin <gosha77788@mail.ru>
2019-11-27 21:50:35 +0300

    cc-006: Покрыть код тестами

--- src/test/java/com/example/cc/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/cc/controller/EmployeeControllerTest.java
@@ -7,7 +7,6 @@ import com.example.cc.service.EmployeeService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
