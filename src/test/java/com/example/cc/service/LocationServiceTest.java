2e292c0aae859315100dd871f8613192f0f950d5
Yura Govorushkin <gosha77788@mail.ru>
2019-12-09 19:21:28 +0300

    cc-006: Покрыть код тестами.

--- src/test/java/com/example/cc/service/LocationServiceTest.java
+++ src/test/java/com/example/cc/service/LocationServiceTest.java
@@ -9,11 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
