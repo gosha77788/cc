3772ebb17f021096bfa4020767b580b0695346f0
Yura Govorushkin <gosha77788@mail.ru>
2020-01-12 12:33:57 +0300

    cc-011:Security, jwt.

--- src/main/java/com/example/cc/service/JwtUserDetailsService.java
+++ src/main/java/com/example/cc/service/JwtUserDetailsService.java
@@ -11,10 +11,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
+@Service
 public class JwtUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
